c64a4137e999741440fe16cd3af150ef3ac43c35
Samantha Capps <samantha@Cheez.local>
2017-03-27 17:46:09 -0400

    second

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,7 +12,55 @@ var database = firebase.database();
 database.ref().on("value", function(snapshot) {
 	$(".yrName").text(snapshot.val().name1);
 	$(".opName").text(snapshot.val().name2);
+	action1();
+	if (snapshot.val().numPlayers == "1") {
+
+	}
+
 })
+
+function action1() {
+	$("#rock").click(rock1());
+	$("#paper").click(paper1());
+	$("#scissors").click(scissors1());
+	$("#lizard").click(lizard1());
+	$("#spock").click(spock1());
+}
+
+function rock1() {
+	database.ref().set({
+	    move1: "rock"
+	});
+	$("#rock").css("background-color", "#FFA500");
+}
+
+function paper1() {
+	database.ref().set({
+	    move1: "paper"
+	});
+	$("#paper").css("background-color", "#FFA500");
+}
+
+function scissors1() {
+	database.ref().set({
+	    move1: "scissors"
+	});
+	$("#scissors").css("background-color", "#FFA500");
+}
+
+function lizard1() {
+	database.ref().set({
+	    move1: "lizard"
+	});
+	$("#lizard").css("background-color", "#FFA500");
+}
+
+function spock1() {
+	database.ref().set({
+	    move1: "spock"
+	});
+	$("#spock").css("background-color", "#FFA500");
+}
 
 window.onbeforeunload = function() {
 	database.ref().on("value", function(snapshot) {
